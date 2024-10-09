1247704cd8c4ff8a7b01bd9f33971567f879fe2f
LuisOrlandoSifuentes <a.22308051281313@cbtis128.edu.mx>
2024-10-09 07:46:38 -0600

    Add files via upload

---
+++ Taller_5J_LOSM/Taller_5J_LOSM/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Taller_5J_LOSM project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller_5J_LOSM.settings')
+
+application = get_asgi_application()
---
+++ Taller_5J_LOSM/Taller_5J_LOSM/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Taller_5J_LOSM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Taller_5J_LOSM.settings')
+
+application = get_wsgi_application()
